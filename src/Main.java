d0678978618e89fff1d14e066e7047414af9faf1
Sven Loman <462549@student.saxion.nl>
2020-11-26 16:19:07 +0100

    Profiles now get read and writed from and to correct files. If the directory does not exist yet, they will be created.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,52 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import org.json.JSONException;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Main {
-    public final int amtOfJiggs = 3;
-    public int numOfProfiles = 1;
+    private String inputPath = "C:" + File.separator + "TSB Profile Generator" + File.separator + "input" + File.separator + "profiles.txt";
+    private String outputPath = "C:" + File.separator + "TSB Profile Generator" + File.separator + "output" + File.separator + "profiles_output.json";
+
+    private final int amtOfJiggs = 3;
+    private int numOfProfiles = 1;
 
     public static void main(String[] args)
     {
-        new Main().run();
+        new Main().setup();
+    }
+
+    /**
+     * This method ensures that the input directory exists. If it does not, the user will be notified
+     */
+    private void setup()
+    {
+        File f = new File(inputPath);
+
+        if(!f.getParentFile().exists()) {
+            try {
+                f.getParentFile().mkdirs();
+                f.createNewFile();
+
+                System.out.println("The profiles.txt file has been created." +
+                        "\nPlease paste the profile details in that file, and run the generator again." +
+                        "\nThe folder is located at: C/TSB Profile Generator/" +
+                        "\nProfiles must be formatted as following:" +
+                        "\n\nProfile name;phone number;cc number;cc expiry date;cc cvc;first name;last name;adress 1;adress 2;country;city;zip" +
+                        "\n\nExample:" +
+                        "\nREVOLUT 1;+3167762212;0000 0000 0000 0000;11 / 25;993;Mals;Kippetje;Zeedijk 61;;Netherlands;Amsterdam;1012AS");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            run();
+        }
+
     }
 
     /**
@@ -27,7 +58,8 @@ public class Main {
 
         //Read cards and details from .txt file
         try {
-            File cardsFile = new File("profiles/profiles.txt");
+            File cardsFile = new File(inputPath);
+
             Scanner reader = new Scanner(cardsFile);
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
@@ -48,12 +80,24 @@ public class Main {
             for(int j=0; j<amtOfJiggs; j++)
             {
                 JSONObject c = getProfileObject(details, j);
-                jsonArray.add(c);
+                jsonArray.put(c);
             }
         }
 
-        System.out.println("Number of profiles created: " + numOfProfiles + "\n" + jsonArray.toString());
+        System.out.println("Number of profiles created: " + (numOfProfiles - 1) + "\n" + jsonArray.toString(2));
 
+        //Write profiles to .json file. This file will be located in C/TSB Profile Generator/output
+        try {
+            File f = new File(outputPath);
+
+            f.getParentFile().mkdirs();
+            f.createNewFile();
+
+            String json = jsonArray.toString(2);
+            Files.write(Paths.get(f.getPath()), json.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -86,7 +130,7 @@ public class Main {
         shipping.put("country", details[9]);
         shipping.put("city", details[10]);
         shipping.put("zip", details[11]);
-        shipping.put("state", null);
+        shipping.put("state", JSONObject.NULL);
 
         //Put billing details
         billing.put("billingSameAsShipping", true);
@@ -95,10 +139,10 @@ public class Main {
         profile.put("cc", card);
         profile.put("shipping", shipping);
         profile.put("billing", billing);
-        profile.put("isJapaneseAddress", null);
-        profile.put("isRussianAddress", null);
-        profile.put("isMexicanAddress", null);
-        profile.put("isPhilippinesAddress", null);
+        profile.put("isJapaneseAddress", JSONObject.NULL);
+        profile.put("isRussianAddress", JSONObject.NULL);
+        profile.put("isMexicanAddress", JSONObject.NULL);
+        profile.put("isPhilippinesAddress", JSONObject.NULL);
         profile.put("date", System.currentTimeMillis());
 
         return profile;
